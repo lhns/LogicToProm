c226256f0772b5ff0536c6cc39d5bd38c1131f79
LolHens <pierrekisters@gmail.com>
2015-07-21 18:14:43 +0200

    moved Pins class into IC

--- src/main/scala/com/dafttech/logic/ic/IC.scala
+++ src/main/scala/com/dafttech/logic/ic/IC.scala
@@ -1,6 +1,7 @@
 package com.dafttech.logic.ic
 
-import com.dafttech.logic.{Pins, Signal}
+import com.dafttech.logic.Signal
+import com.dafttech.logic.ic.IC.Pins
 
 /**
  * Created by LolHens on 21.07.2015.
@@ -36,4 +37,26 @@ class IC protected(inPins: Int, outPins: Int) {
 
 object IC {
   def apply(inPins: Int, outPins: Int): IC = new IC(inPins, outPins)
+
+  abstract class Pins(val length: Int) {
+    private val arrayIn = new Array[Signal](length)
+
+    private val arrayOut = new Array[Signal](length)
+
+    for (i <- 0 until length) arrayOut(i) = Signal(arrayIn(i) match {
+      case null => false
+      case signal => signal.value
+    })
+
+    def update(i: Int, signalProvider: Signal) = i match {
+      case _ if (i < 0 || i >= length) => throw new IndexOutOfBoundsException()
+      case _ => arrayIn(i) = signalProvider
+    }
+
+    def apply(i: Int): Signal = i match {
+      case _ if (i < 0 || i >= length) => throw new IndexOutOfBoundsException()
+      case _ => arrayOut(i)
+    }
+  }
+
 }
