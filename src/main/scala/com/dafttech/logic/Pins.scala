f722c09fb65ec76441cc3a86e4ebf0ec0171220b
LolHens <pierrekisters@gmail.com>
2015-07-21 18:13:08 +0200

    reordered methods in Pins

--- src/main/scala/com/dafttech/logic/Pins.scala
+++ src/main/scala/com/dafttech/logic/Pins.scala
@@ -4,6 +4,15 @@ package com.dafttech.logic
  * Created by LolHens on 21.07.2015.
  */
 abstract class Pins(val length: Int) {
+  private val arrayIn = new Array[Signal](length)
+
+  private val arrayOut = new Array[Signal](length)
+
+  for (i <- 0 until length) arrayOut(i) = Signal(arrayIn(i) match {
+    case null => false
+    case signal => signal.value
+  })
+
   def update(i: Int, signalProvider: Signal) = i match {
     case _ if (i < 0 || i >= length) => throw new IndexOutOfBoundsException()
     case _ => arrayIn(i) = signalProvider
@@ -13,13 +22,4 @@ abstract class Pins(val length: Int) {
     case _ if (i < 0 || i >= length) => throw new IndexOutOfBoundsException()
     case _ => arrayOut(i)
   }
-
-  private val arrayIn = new Array[Signal](length)
-
-  private val arrayOut = new Array[Signal](length)
-
-  for (i <- 0 until length) arrayOut(i) = Signal(arrayIn(i) match {
-    case null => false
-    case signal => signal.value
-  })
 }
