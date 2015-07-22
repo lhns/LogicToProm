6b82bd207caddc80f9a969153b14230b18081716
LolHens <pierrekisters@gmail.com>
2015-07-22 20:20:26 +0200

    added inverter

---
+++ src/main/scala/com/dafttech/logic/ic/Inverter.scala
@@ -0,0 +1,12 @@
+package com.dafttech.logic.ic
+
+/**
+ * Created by LolHens on 22.07.2015.
+ */
+class Inverter private(size: Int) extends IC(size, size) {
+  for (i <- 0 until size) out(i) = !in(i)
+}
+
+object Inverter {
+  def apply(size: Int) = new Inverter(size)
+}
